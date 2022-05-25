b2fa9342273cb3b4bcf05db8e571a51d9019c19f
Joey Zhao <zhaoyi0113@gmail.com>
2022-05-25 13:11:40 +1000

    send transaction event

--- internal/transactionService.go
+++ internal/transactionService.go
@@ -5,15 +5,42 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge/types"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/google/uuid"
 )
+
+var entityUuid = "5262a830-7b7a-41d4-ba81-062459d36992"
+var siteUuid = "3c4ed0c6-a028-4c9f-a830-84c7cdde5db9"
 
 type TransactionRequest struct {
 	Amount int
+}
+
+type TransactionInitiated struct {
+	Amount          int    `json:"amount"`
+	TransactionUuid string `json:"transactionUuid"`
+	Timestamp       string `json:"timestamp"`
+	TimestampUtc    string `json:"timestampUtc"`
+	EntityUuid      string `json:"entityUuid"`
+	SiteUuid        string `json:"siteUuid"`
+	Type            string `json:"type"`
+}
+
+type TransactionApproved struct {
+	TransactionUuid     string `json:"transactionUuid"`
+	EntityUuid          string `json:"entityUuid"`
+	SiteUuid            string `json:"siteUuid"`
+	Timestamp           string `json:"timestamp"`
+	TimestampUtc        string `json:"timestampUtc"`
+	ResponseCode        string `json:"responseCode"`
+	ResponseDescription string `json:"responseDescription"`
+	ApprovalCode        string `json:"approvalCode"`
+	Rrn                 string `json:"rrn"`
 }
 
 func ProcessTransactionRequest(request []byte) {
@@ -32,11 +59,45 @@ func ProcessTransactionRequest(request []byte) {
 	}
 	client := eventbridge.NewFromConfig(cfg)
 
+	initiatedEvent := getInitiatedEvent(transactionRequest.Amount)
+	approvedEvent := getApprovedEvent()
+
+	sendEvent(client, initiatedEvent)
+	sendEvent(client, approvedEvent)
+}
+
+func getInitiatedEvent(amount int) string {
+	initiated := TransactionInitiated{
+		TransactionUuid: uuid.NewString(),
+		Amount:          amount,
+		Timestamp:       time.Now().Format(time.RFC3339),
+		TimestampUtc:    time.Now().UTC().Format("2006-01-02T15:04:05Z07:00"),
+		Type:            "PURCHASE",
+		EntityUuid:      entityUuid,
+		SiteUuid:        siteUuid,
+	}
+	bytes, _ := json.Marshal(initiated)
+	return string(bytes)
+}
+
+func getApprovedEvent() string {
+	approved := TransactionApproved{
+		EntityUuid:      entityUuid,
+		SiteUuid:        siteUuid,
+		TransactionUuid: uuid.NewString(),
+		Timestamp:       time.Now().Format(time.RFC3339),
+		TimestampUtc:    time.Now().UTC().Format("2006-01-02T15:04:05Z07:00"),
+	}
+	bytes, _ := json.Marshal(approved)
+	return string(bytes)
+}
+
+func sendEvent(client *eventbridge.Client, event string) {
 	output, err := client.PutEvents(context.TODO(), &eventbridge.PutEventsInput{
 		Entries: []types.PutEventsRequestEntry{
 			{
 				EventBusName: aws.String("dev-eventBus-global"),
-				Detail:       aws.String("{ \"key1\": \"value1\", \"key2\": \"value2\" }"),
+				Detail:       aws.String(event),
 				DetailType:   aws.String("pgs.Transaction.Initiated"),
 				Source:       aws.String("pgs"),
 			},
